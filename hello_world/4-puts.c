1fb32a33f76104ae18386296609226b0dee3f9fd
Ilian <12091@holbertonstudents.com>
2025-10-20 11:10:59 +0000

    e

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle)
+#include <stdio.h>
+/*
+ * simple command that print \"Preogramming is like a multilingual puzzle\" to the user
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
