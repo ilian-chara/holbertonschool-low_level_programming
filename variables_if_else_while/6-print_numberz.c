71a99954f28d4b6061d1b824eb3b623cc9278e1e
ilian <12091@holbertonstudents.com>
2025-10-21 13:35:02 +0000

    e

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	putchar (num);
+	putchar ('\n');
+	return (0);
+}
