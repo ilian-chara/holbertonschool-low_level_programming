d3d55cf6339f7823f9df4686b596649d29bdf820
ilian <12091@holbertonstudents.com>
2025-10-21 13:44:24 +0000

    e

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	char zaza;
+
+		for (zaza = 'z'; zaza <= 'a'; zaza++)
+			putchar (zaza);
+	putchar ('\n');
+	return (0);
+}
