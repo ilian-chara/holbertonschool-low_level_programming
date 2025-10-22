198613883c2f10b6bda4f9203b7a92fa0be585f0
ilian <12091@holbertonstudents.com>
2025-10-22 13:14:26 +0000

    e

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _islower - if lowercase, = 1
- * Return: always (0)
- * @c - is a variable
+ *_islower -Verif si un caractere est en min
+ *@c: Le caractere a verifier (code ASCII)
+ *Return: c'est 1 si c'est une min et 0 sinon
  */
-
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
 }
