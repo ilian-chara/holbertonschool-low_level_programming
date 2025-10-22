72de5dd3bedd999b774d04617e0ef7fc75785f06
ilian <12091@holbertonstudents.com>
2025-10-22 10:05:35 +0000

    e

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet  - print_alphabet
+ * Return: always (0)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+		_putchar('\n');
+}
