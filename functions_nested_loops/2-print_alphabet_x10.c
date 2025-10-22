940f27c7f59269c1111eaba4c202d1acc5f48055
ilian <12091@holbertonstudents.com>
2025-10-22 12:06:03 +0000

    e

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - ntm betty
+ * Return: always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int multi;
+
+	for (multi = 0; multi < 10; multi++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+	}
+	_putchar('\n');
+}
