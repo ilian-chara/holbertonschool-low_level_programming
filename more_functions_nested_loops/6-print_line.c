7ae5c1443b7c5d3a8330fe041a6b2495f0ec68f0
ilian <12091@holbertonstudents.com>
2025-10-27 18:30:57 +0000

    e

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,11 +8,16 @@ void print_line(int n)
 {
 	int z;
 
-	if (n != 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
-		return;
 	}
-	for (z = 0; z < n; z++)
-		_putchar('_');
+	else
+	{
+		for (z = 0; z < n; z++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
