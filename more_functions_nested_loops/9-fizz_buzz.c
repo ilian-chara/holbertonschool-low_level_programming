3b0b932533f472ff5dc9106dde8cc7d040a5eebf
ilian <12091@holbertonstudents.com>
2025-10-28 09:41:50 +0000

    e

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point of function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int f;
+
+	for (f = 0; f <= 100; f++)
+	{
+		if (f % 3 == 0 && f % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (f % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (f % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", f);
+		}
+	}
+	return (0);
+}
