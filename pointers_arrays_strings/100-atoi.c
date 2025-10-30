9fca048184547b8551778551658d1e4b2bf0a715
ilian <12091@holbertonstudents.com>
2025-10-30 12:01:30 +0000

    e

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -24,20 +24,8 @@ int _atoi(char *s)
 	}
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		int dig = s[i] - '0';
-
-		if (si == 1)
-		{
-			if (n > (2147483647 - dig) / 10)
-				return (2147483647);
-		}
-		else
-		{
-			if (n < (-2147483648 + dig) / 10)
-				return (-2147483648);
-		}
-		n = n * 10 + si * dig;
+		n = n * 10 + (s[i] - '0');
 		i++;
 	}
-	return (n);
+	return (n * si);
 }
