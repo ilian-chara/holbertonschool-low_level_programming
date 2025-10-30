e3c51776d767bb60e96d3fe93881397ba87c2540
ilian <12091@holbertonstudents.com>
2025-10-30 11:48:37 +0000

    e

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _atoi - function name
+ * @s: pointer
+ * Return: num multiplied by si
+ *
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int si = 1;
+	int n = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			si = -si;
+		else if (s[i] == '+')
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
+			break;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		n = n * 10 + (s[i] - '0');
+
+		i++;
+	}
+	return (n * si);
+}
