2dc2eb84923d17de6586a90ae5e2169440b4f6af
ilian <12091@holbertonstudents.com>
2025-11-05 09:13:55 +0000

    e

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - print the program name
+* @argc: unused
+* @argv: array of strings
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
